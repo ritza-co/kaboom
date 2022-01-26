ebdf1e7682edb14fffe4677321b3a87c99c961a7
tga <tga@space55.xyz>
2022-01-25 19:50:08 -0500

    Refactor assets loading (#496)
    * implement sprite asset bucket
    
    * font working
    
    * sound working
    
    * fix aseprite & pedit loader
    
    * expose load progress
    
    * customizable loading screen
    
    * fix loadSpriteAtlas() for atlas data literal
    
    * rm console.log
    
    * clean frame sequencing
    
    * misc

--- src/types.ts
+++ src/types.ts
@@ -1076,6 +1076,7 @@ export interface KaboomCtx {
 	 * ```
 	 */
 	load<T>(l: Promise<T>): void,
+	loadProgress<T>(): number,
 	/**
 	 * Get the width of game.
 	 *
@@ -2033,6 +2034,12 @@ export interface KaboomOpt {
 	 */
 	touchToMouse?: boolean,
 	/**
+	 * If kaboom should render a default loading screen when assets are not fully ready (default true).
+	 *
+	 * @since v2001.0
+	 */
+	loadingScreen?: boolean,
+	/**
 	 * If import all kaboom functions to global (default true).
 	 */
 	global?: boolean,
@@ -2276,6 +2283,13 @@ export interface SpriteAnimPlayOpt {
 	 * Runs when this animation ends.
 	 */
 	onEnd?: () => void,
+}
+
+export interface PeditFile {
+	width: number,
+	height: number,
+	frames: string[],
+	anims: SpriteAnims,
 }
 
 /**
