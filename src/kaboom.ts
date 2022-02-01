0f5b49f651df05499c50395347cc51f4ba13ada9
tga <tga@space55.xyz>
2022-02-01 13:41:27 -0500

    fix error logging

--- src/kaboom.ts
+++ src/kaboom.ts
@@ -5422,7 +5422,7 @@ function handleErr(msg: string) {
 }
 
 window.addEventListener("error", (e) => {
-	handleErr(e.error.message);
+	handleErr(e.error instanceof Error ? e.error.message : e.error);
 });
 
 window.addEventListener("unhandledrejection", (e) => {
