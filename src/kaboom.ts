2fc510d89ded8759532ffbc982624b8ac2d395ab
tga <tga@space55.xyz>
2022-02-03 22:54:15 -0500

    fix text comp font not working

--- src/kaboom.ts
+++ src/kaboom.ts
@@ -2747,7 +2747,7 @@ const debug: Debug = {
 	stepFrame: updateFrame,
 	drawCalls: () => gfx.drawCalls,
 	clearLog: () => game.logs = [],
-	log: (msg) => game.logs.unshift(`${gopt.logTime ? `[${time().toFixed(2)}].time ` : ""}[${msg.toString ? msg.toString().trimEnd() : msg}].${msg instanceof Error ? "error" : "info"}`),
+	log: (msg) => game.logs.unshift(`${gopt.logTime ? `[${time().toFixed(2)}].time ` : ""}[${msg?.toString ? msg.toString().trimEnd() : msg}].${msg instanceof Error ? "error" : "info"}`),
 	error: (msg) => debug.log(new Error(msg.toString ? msg.toString() : msg as string)),
 	curRecording: null,
 	get paused() {
@@ -4225,11 +4225,11 @@ function text(t: string, opt: TextCompOpt = {}): TextComp {
 			text: obj.text + "",
 			size: obj.textSize,
 			font: obj.font,
-			width: opt.width && obj.width,
-			letterSpacing: obj.letterSpacing,
-			lineSpacing: obj.lineSpacing,
-			transform: obj.transform,
-			styles: obj.styles,
+			width: opt.width,
+			letterSpacing: opt.letterSpacing,
+			lineSpacing: opt.lineSpacing,
+			transform: opt.transform,
+			styles: opt.styles,
 		});
 
 		obj.width = ftext.width / (obj.scale?.x || 1);
@@ -5489,6 +5489,7 @@ loadFont(
 	apl386Src,
 	45,
 	74,
+	{ filter: "linear", }
 );
 
 loadFont(
@@ -5496,6 +5497,7 @@ loadFont(
 	apl386oSrc,
 	45,
 	74,
+	{ filter: "linear", }
 );
 
 loadFont(
